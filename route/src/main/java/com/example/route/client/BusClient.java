d960a0dffca7e5b290ba15177bd31d8abdbd9421
Ahnjaewon <vutus5@naver.com>
2023-09-18 19:44:34 +0900

    update

--- route/src/main/java/com/example/route/client/BusClient.java
+++ route/src/main/java/com/example/route/client/BusClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("BUS-SERVICE")
+@FeignClient("RESERVATION-SERVICE")
 public interface BusClient {
 
 
