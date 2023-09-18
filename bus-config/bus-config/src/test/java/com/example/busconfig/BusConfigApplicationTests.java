5966f5758f7c0ce0fbae3e9f82b77a2da16d178a
Playdata <vutus5@naver.com>
2023-09-18 20:03:08 +0900

    user auth

---
+++ bus-config/bus-config/src/test/java/com/example/busconfig/BusConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
